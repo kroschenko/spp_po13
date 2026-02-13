ee4b340be9d25bb6702e2c7ad70870b887a48416
DobryiZlodey <tarasjuk45@gmail.com>
2026-02-13 10:02:20 +0300

    fix: lint for 1.2 line break

--- reports/Tarasiuk/1/src/1.2.py
+++ reports/Tarasiuk/1/src/1.2.py
@@ -4,7 +4,9 @@ class Solution:
         min_word = min(strs)
         for i in enumerate(min_word):
             for j in enumerate(strs):
-                if min_word[i] == strs[j][i]: continue
+                if min_word[i] == strs[j][i]: 
+                    continue
                 return word
             word += min_word[i]
         return word
+    
