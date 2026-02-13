bf950083c8adcdfe25d70baff5516bb4d8c56bd2
DobryiZlodey <tarasjuk45@gmail.com>
2026-02-13 11:01:40 +0300

    fix: lint

--- reports/Tarasiuk/1/src/1.2.py
+++ reports/Tarasiuk/1/src/1.2.py
@@ -1,10 +1,14 @@
 def longestCommonPrefix(strs):
+    if not strs:
+        return ""
+        
     word = ""
     min_word = min(strs, key=len)
-    for i, number in enumerate(min_word):
-        for j, value in enumerate(strs):
-            if min_word[i] == strs[j][i]:
-                continue
-            return word
-        word += min_word[i]
+    
+    for i, ch in enumerate(min_word):
+        for j, _ in enumerate(strs):
+            if ch != strs[j][i]:
+                return word
+        word += ch
+    
     return word
