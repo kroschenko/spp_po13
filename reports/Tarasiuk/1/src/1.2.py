007444dda7215d47afd57a9d9852e87693fb9438
DobryiZlodey <tarasjuk45@gmail.com>
2026-02-13 09:58:03 +0300

    fix: lint for 1.2

--- reports/Tarasiuk/1/src/1.2.py
+++ reports/Tarasiuk/1/src/1.2.py
@@ -1,10 +1,10 @@
 class Solution:
     def longestCommonPrefix(self, strs: List[str]) -> str:
-        word=""
-        min_word=min(strs)
+        word = ""
+        min_word = min(strs)
         for i in enumerate(min_word):
             for j in enumerate(strs):
-                if min_word[i]==strs[j][i]: continue
+                if min_word[i] == strs[j][i]: continue
                 return word
-            word+=min_word[i]
+            word += min_word[i]
         return word
