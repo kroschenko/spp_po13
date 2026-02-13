66e198ca09b4c0bb9d43738f59704b476114da04
DobryiZlodey <tarasjuk45@gmail.com>
2026-02-13 10:21:47 +0300

    fix: refactor 1.2

--- reports/Tarasiuk/1/src/1.2.py
+++ reports/Tarasiuk/1/src/1.2.py
@@ -1,12 +1,10 @@
-class Solution:
-    def longestCommonPrefix(strs):
-        word = ""
-        min_word = min(strs)
-        for i in enumerate(min_word):
-            for j in enumerate(strs):
-                if min_word[i] == strs[j][i]:
-                    continue
-                return word
-            word += min_word[i]
-        return word
-    
+def longestCommonPrefix(strs):
+    word = ""
+    min_word = min(strs, key=len)
+    for i, number in enumerate(min_word):
+        for j, value in enumerate(strs):
+            if min_word[i] == strs[j][i]:
+                continue
+            return word
+        word += min_word[i]
+    return word
