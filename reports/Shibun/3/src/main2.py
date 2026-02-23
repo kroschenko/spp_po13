81d2a91eace71dfdec65fc46a96dc8dfc0e9396b
Dima Shibun <Shibun2006@gmail.com>
2026-02-23 22:05:19 +0300

    feat(lab3): реализованы паттерны проектирования

---
+++ reports/Shibun/3/src/main2.py
@@ -0,0 +1,49 @@
+from abc import ABC, abstractmethod
+
+class Card(ABC):
+    @abstractmethod
+    def info(self) -> str:
+        pass
+
+
+class BaseCard(Card):
+    def info(self) -> str:
+        return "Универсальная электронная карта"
+
+
+class CardDecorator(Card):
+    def __init__(self, card: Card):
+        self.card = card
+
+    @abstractmethod
+    def info(self) -> str:
+        pass
+
+
+class PassportDecorator(CardDecorator):
+    def info(self) -> str:
+        return self.card.info() + ", паспортные данные"
+
+
+class InsuranceDecorator(CardDecorator):
+    def info(self) -> str:
+        return self.card.info() + ", страховой полис"
+
+
+class BankCardDecorator(CardDecorator):
+    def info(self) -> str:
+        return self.card.info() + ", банковская карта"
+
+
+class TransportDecorator(CardDecorator):
+    def info(self) -> str:
+        return self.card.info() + ", транспортная карта"
+
+
+if __name__ == "__main__":
+    card = BaseCard()
+    card = PassportDecorator(card)
+    card = InsuranceDecorator(card)
+    card = BankCardDecorator(card)
+
+    print(card.info())
