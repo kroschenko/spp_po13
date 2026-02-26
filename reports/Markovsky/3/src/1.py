bdf36965aea3849ff7f3019d9be99b7cfea5046d
Carrotist <romark99@gmail.com>
2026-02-26 09:34:15 +0300

    fix: kwargs instead of positional arguments for 1.py

--- reports/Markovsky/3/src/1.py
+++ reports/Markovsky/3/src/1.py
@@ -1,11 +1,11 @@
 class Smartphone:
-    def __init__(self, model, screen_size, ram, storage, camera_mp, battery):
-        self.model = model
-        self.screen_size = screen_size
-        self.ram = ram
-        self.storage = storage
-        self.camera_mp = camera_mp
-        self.battery = battery
+    def __init__(self, **kwargs):
+        self.model = kwargs.get('model', 'Неизвестная модель')
+        self.screen_size = kwargs.get('screen_size', 0)
+        self.ram = kwargs.get('ram', 0)
+        self.storage = kwargs.get('storage', 0)
+        self.camera_mp = kwargs.get('camera_mp', 0)
+        self.battery = kwargs.get('battery', 0)
         self.is_on = False
 
     def __str__(self):
