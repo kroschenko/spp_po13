beb3e27466249c906986c4fc5f814dc2c70f5292
anita <annapotapcuk486@gmail.com>
2026-04-03 10:08:18 +0300

    fix: fix pylint and black issues in models.py

--- reports/Potapchuk/5/scr/models.py
+++ reports/Potapchuk/5/scr/models.py
@@ -1,17 +1,24 @@
-from sqlalchemy import Column, Integer, String, ForeignKey, Numeric, Date, DateTime
+from sqlalchemy import Column, ForeignKey, Integer, Numeric, String
 from sqlalchemy.orm import relationship
+
 from database import Base
+
 
 class Category(Base):
     __tablename__ = "categories"
+
     id = Column(Integer, primary_key=True)
     name = Column(String)
+
     items = relationship("Item", back_populates="category")
+
 
 class Item(Base):
     __tablename__ = "items"
+
     id = Column(Integer, primary_key=True)
     title = Column(String)
     price_per_day = Column(Numeric)
+
     category_id = Column(Integer, ForeignKey("categories.id"))
     category = relationship("Category", back_populates="items")
