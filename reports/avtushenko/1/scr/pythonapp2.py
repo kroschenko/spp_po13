a03f0a1a59a34d63f81c31e5c95fed830ebf89b7
Y4rosl4v <145491610+Y4rosl4v@users.noreply.github.com>
2026-04-10 10:01:33 +0300

    feat: task2 for lab1

---
+++ reports/avtushenko/1/scr/pythonapp2.py
@@ -0,0 +1,20 @@
+﻿def two_sum(nums, target):
+    seen = {}
+    for i, num in enumerate(nums):
+        complement = target - num
+        if complement in seen:
+            return [seen[complement], i]
+        seen[num] = i
+    return None
+
+
+# Ввод данных
+nums = list(map(int, input("Введите числа через пробел: ").split()))
+target = int(input("Введите целевое число: "))
+
+result = two_sum(nums, target)
+if result:
+    print(f"Индексы: {result}")
+    print(f"Проверка: {nums[result[0]]} + {nums[result[1]]} = {target}")
+else:
+    print("Решение не найдено")
