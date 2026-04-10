23d14f934f662ca882f71fd72b706402bbff5b44
Y4rosl4v <145491610+Y4rosl4v@users.noreply.github.com>
2026-04-10 10:07:29 +0300

    style: rename values

--- reports/avtushenko/1/scr/pythonapp2.py
+++ reports/avtushenko/1/scr/pythonapp2.py
@@ -1,7 +1,7 @@
-﻿def two_sum(nums, target):
+﻿def two_sum(nums_arr, target_sum):
     seen = {}
-    for i, num in enumerate(nums):
-        complement = target - num
+    for i, num in enumerate(nums_arr):
+        complement = target_sum - num
         if complement in seen:
             return [seen[complement], i]
         seen[num] = i
