3a64c592a5e921cdd1bffba8c5a2af952efa06a8
anita <annapotapcuk486@gmail.com>
2026-04-16 22:30:10 +0300

    fix: correct lab7

--- reports/Potapchuk/7/scr/lab7_task2.py
+++ reports/Potapchuk/7/scr/lab7_task2.py
@@ -6,24 +6,22 @@ from PIL import ImageGrab
 class DragonFractal:
     def __init__(self, root):
         self.root = root
-        self.root.title("Лабораторная 7 - Задание 2 (Дракон)")
-
+        self.root.title("Задание 2")
         self.menu = tk.Frame(root)
         self.menu.pack(pady=10)
-
         tk.Label(self.menu, text="Глубина:").pack(side=tk.LEFT)
         self.depth_spn = tk.Spinbox(self.menu, from_=1, to=14, width=5)
         self.depth_spn.pack(side=tk.LEFT, padx=5)
         self.depth_spn.delete(0, "end")
-        self.depth_spn.insert(0, "10")
-
+        self.depth_spn.insert(0, "1")
         tk.Button(self.menu, text="Отрисовать", command=self.start_draw).pack(side=tk.LEFT)
         tk.Button(self.menu, text="Скриншот", command=self.save_img).pack(side=tk.LEFT, padx=5)
-
         self.canvas = tk.Canvas(root, width=700, height=600, bg="#1e1e1e")
         self.canvas.pack(padx=10, pady=10)
 
+    # pylint: disable=too-many-positional-arguments
     def draw_dragon(self, x1, y1, x2, y2, k):
+        """Рекурсивное построение фрактала."""
         if k == 0:
             self.canvas.create_line(x1, y1, x2, y2, fill="#00FF7F", width=1)
         else:
