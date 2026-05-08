1c89de81009de3bf393238d4ed0cfdedaffd95b9
MrBSoD <8244984@gmail.com>
2026-05-08 09:28:21 +0300

    feat:completed tasks

--- reports/Bosak/lab1/scr/1.py
+++ reports/Bosak/lab1/scr/1.py
@@ -1,6 +1,7 @@
 def sum_squares_negative(numbers):
     return sum(x**2 for x in numbers if x < 0)
 
+
 numbers = list(map(int, input().split()))
 result = sum_squares_negative(numbers)
 print(result)
--- reports/Bosak/lab1/scr/2.py
+++ reports/Bosak/lab1/scr/2.py
@@ -1,17 +1,18 @@
 def is_valid_brackets(s):
-    brackets = {')': '(', '}': '{', ']': '['}
+    brackets = {")": "(", "}": "{", "]": "["}
     stack = []
-    
+
     for char in s:
-        if char in '({[':
+        if char in "({[":
             stack.append(char)
-        elif char in ')}]':
+        elif char in ")}]":
             if not stack or stack[-1] != brackets[char]:
                 print(False)
                 return
             stack.pop()
-    
+
     print(len(stack) == 0)
+
 
 s = input()
 is_valid_brackets(s)
